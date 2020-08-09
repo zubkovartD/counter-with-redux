3bb16dc691d22038e6eb5dfdbd9202976eee10a9
zubkovartD <36312132+zubkovartD@users.noreply.github.com>
2020-08-09 14:15:32 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { createStore } from 'redux'
+
+function counter (state = 0, action) {
+  switch (action.type) {
+    case 'INCREMENT': {
+      return state + 1;
+    }
+    case 'DECREMENT': {
+      return state - 1;
+    }
+    default: {
+      return state;
+    }
+  }
+}
+
+export const store = createStore(counter);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter />
     </div>
   );
 }
 
 export default App;
+
+class Counter extends React.Component {
+  render(){
+    return(
+      <div>
+        <button onClick={ () => store.dispatch({type: 'DECREMENT'}) }> - </button>
+        {store.getState()}
+        <button onClick={ () => store.dispatch({type: 'INCREMENT'}) }> + </button>
+      </div>
+    );
+  }
+}
