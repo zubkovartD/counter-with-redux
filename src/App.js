4bb337c705c1188e90528d9f10e777ae65a89bbb
zubkovartD <36312132+zubkovartD@users.noreply.github.com>
2020-09-07 21:57:32 +0300

    .

--- src/App.js
+++ src/App.js
@@ -2,22 +2,9 @@ import React from 'react';
 import './App.css';
 import { createStore } from 'redux'
 import Counter from './Counter'
+import reducer from './reducer'
 
-function counter (state = 0, action) {
-  switch (action.type) {
-    case 'INCREMENT': {
-      return state + 1;
-    }
-    case 'DECREMENT': {
-      return state - 1;
-    }
-    default: {
-      return state;
-    }
-  }
-}
-
-export const store = createStore(counter);
+export const store = createStore(reducer);
 
 function App() {
   return (
--- src/Counter.js
+++ src/Counter.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import { connect } from 'react-redux'
+import { increment, decrement } from './Actions'
 
 class Counter extends React.Component {
     render(){
+        const {count, increment, decrement} = this.props
         return(
             <div>
-                <button onClick={ () => this.props.dispatch({type: 'DECREMENT'}) }> - </button>
-                    {this.props.count}
-                <button onClick={ () => this.props.dispatch({type: 'INCREMENT'}) }> + </button>
+                <button onClick={() => decrement(5)}> -5 </button>
+                <button onClick={() => decrement(1)}> -1 </button>
+                    {count}
+                <button onClick={() => increment(1)}> +1 </button>
+                <button onClick={() => increment(5)}> +5 </button>
             </div>
         );
     }
 }
 
-export default connect(state => ({count: state}))(Counter)
+const mapStateToProps = state => ({count: state})
+
+
+
+export default connect(mapStateToProps, { increment, decrement })(Counter)
